b4acd233ed1abc82b931b4d633c030c14f0349ca
bernagl <luisb.galo@gmail.com>
2018-10-25 01:55:04 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,112 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
+  resetState = () => ({
+    cards: [],
+    numberSelected: null,
+    selectedPosition: null,
+    points: 0
+  })
+
+  state = this.resetState()
+
+  componentDidMount() {
+    this.setCards()
+  }
+
+  setCards = () => {
+    const cards1 = [...new Array(6)]
+    const cards2 = [...new Array(6)]
+    cards1.map((e, i) => this.fillArray(cards1, i))
+    cards2.map((e, i) => this.fillArray(cards2, i))
+    const cards = cards1.concat(cards2)
+    cards.sort(() => Math.random() - 0.5)
+    this.setState({ ...this.resetState(), cards })
+  }
+
+  fillArray = (array, i) => {
+    const number = this.getRandomNumber()
+    if (array.find(e => (e ? e['number'] === number : false)))
+      this.fillArray(array, i)
+    else array[i] = { number, status: false }
+  }
+
+  getRandomNumber = () => Math.floor(Math.random() * 6 + 1)
+
+  handleCard = (number, position) => {
+    const { selectedPosition, numberSelected } = this.state
+    if (!numberSelected) {
+      this.setState({ numberSelected: number, selectedPosition: position })
+    } else {
+      if (number === numberSelected && selectedPosition !== position) {
+        this.setState(({ cards, points }) => {
+          const cardsCopy = [...cards]
+          cardsCopy[selectedPosition] = {
+            ...cardsCopy[selectedPosition],
+            status: true
+          }
+          cardsCopy[position] = { ...cardsCopy[position], status: true }
+          return {
+            cards: cardsCopy,
+            selected: null,
+            selectedPosition: null,
+            numberSelected: null,
+            points: points + 1
+          }
+        })
+      } else {
+        this.setState(
+          {
+            selectedPosition: position,
+            numberSelected: null
+          },
+          () =>
+            setTimeout(() => {
+              this.setState({ selectedPosition: null })
+            }, 200)
+        )
+      }
+    }
+  }
+
   render() {
+    const { cards, selectedPosition, points } = this.state
+    const didFinish = points === cards.length / 2
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        {didFinish && (
+          <div className="congratulations">
+            <div className="congratulations__box" />
+            <button onClick={this.setCards} className="congratulations__button">
+              Play again!
+            </button>
+          </div>
+        )}
+        <div className="memorama">
+          {cards
+            ? cards.map(({ number, status }, i) => (
+                <div
+                  className={`memorama__card ${
+                    status ? 'active' : 'inactive'
+                  } ${selectedPosition === i ? 'selected' : ''}`}
+                  key={i}
+                  onClick={() =>
+                    !status && selectedPosition !== i
+                      ? this.handleCard(number, i)
+                      : alert(`Don't know how to play? Choose another card`)
+                  }
+                >
+                  {selectedPosition === i || status ? number : ''}
+                </div>
+              ))
+            : 'Loading cards'}
+          <div className="memorama__credits">
+            <span>@bernagl</span>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
